5011b59e0ba69577fb2e5a892e6c3a36e003187f
W55296 <w55296@student.wsiz.rzeszow.pl>
2018-05-26 14:34:25 +0200

    Pobieranie danych

--- app/src/main/java/com/example/stud/musicapp/topsongs/TopSongsActivity.java
+++ app/src/main/java/com/example/stud/musicapp/topsongs/TopSongsActivity.java
@@ -1,9 +1,19 @@
 package com.example.stud.musicapp.topsongs;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.example.stud.musicapp.R;
+import com.example.stud.musicapp.api.ApiService;
+import com.example.stud.musicapp.api.TrendingList;
+import com.google.gson.Gson;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class TopSongsActivity extends AppCompatActivity {
 
@@ -13,7 +23,25 @@ public class TopSongsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_top_songs);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Call<TrendingList> trendingListCall = ApiService. getService ().getTrendingList( "us" ,
+                "itunes" , "singles" );
+        trendingListCall.enqueue( new Callback<TrendingList>() {
+            @Override
+            public void onResponse( @NonNull Call<TrendingList> call, @NonNull
+                    Response<TrendingList> response) {
+                TrendingList trendingList = response.body();
+                Log. d ( "TAG" , new Gson().toJson(trendingList));
+            }
+            @Override
+            public void onFailure( @NonNull Call<TrendingList> call, Throwable t) {
+                Toast. makeText (TopSongsActivity. this , "Blad pobierania danych: " +
+                        t.getLocalizedMessage(), Toast. LENGTH_SHORT ).show();
+            }
+        });
     }
+
+
 
     @Override
     public boolean onSupportNavigateUp() {
